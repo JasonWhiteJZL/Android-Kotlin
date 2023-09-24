168a2dadec7ee9c8a52b390730b3d0f7c7febf70
JasonWhiteJZL <jzlcmd@gmail.com>
2023-09-24 22:17:05 +0800

    frist commit

---
+++ app/src/main/java/com/jw/myapplication/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.jw.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        //full the screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //jump main page
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
---
+++ app/src/main/java/com/log.md
@@ -0,0 +1,16 @@
+20230614
+Splash do not complete,it need a icon,page use to xml file,main use to Compose page.
+
+20230912
+https://juejin.cn/post/7092217592328224781
+设计模式，算法 记得学习
+WIFI编译
+继续完成compose的tabbar
+
+20230920
+设计模式，算法 记得学习
+WIFI编译
+继续完成compose的tabbar
+测试是否可以敲打中文字符
+创建git仓库，提交到github上去
+创建分支，win系统上一个，linux上一个
