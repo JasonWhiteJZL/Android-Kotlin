ab398b2c26f2640700db347f6286ae67be7fdcc4
JasonWhiteJZL <jzlcmd@gmail.com>
2023-12-25 22:57:23 +0800

    简易的登录逻辑已完成的差不多，UI还没有调试，明天将登录流程写完（Demo），先将整个逻辑框架搭建出来再完善UI界面和方法

--- app/src/main/java/com/jw/myapplication/LoginActivity.kt
+++ app/src/main/java/com/jw/myapplication/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.jw.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +22,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.jw.myapplication.method.Login
 import com.jw.myapplication.ui.theme.JwmusicTheme
 
 class LoginActivity : ComponentActivity() {
@@ -42,15 +47,42 @@ class LoginActivity : ComponentActivity() {
 @Composable
 fun Login()
 {
+    val context = LocalContext.current
     var account by remember { mutableStateOf("") }
     var pwd by remember { mutableStateOf("") }
+    val openDialog = remember { mutableStateOf(true) }
+    var login : Login = Login()
     Column()
     {
         TextField(
             value = account,
             onValueChange = {account = it},
-
+            label = {Text("Account")}
         )
+
+        TextField(
+            value = pwd,
+            onValueChange = {pwd = it},
+            label = {Text("PassWord")}
+        )
+
+        Button(
+            onClick = {
+                      if(login.login(account,pwd))
+                      {
+                          val intent = Intent(context,MainActivity::class.java)
+                          context.startActivity(intent)
+                      }else{
+                          //TODO 这里使用Compose的消息弹窗
+
+                      }
+
+        },
+            )
+        {
+            
+        }
+
     }
 }
 
